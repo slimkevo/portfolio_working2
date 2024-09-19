fd8dfe143fb1b03af12324ee40af8acfef6f1e84
slimkevo <kevinkamerling2021@gmail.com>
2024-09-19 00:27:53 -0400

    final working

---
+++ personal-portfolio/src/components/Newsletter.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+}
